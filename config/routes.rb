7ad64940cc4dc7bd1b8a15d6dfea6e1d34e8210d
gmotzespina <gmotzespina@icloud.com>
2018-09-24 18:07:27 -0500

    Add login and signup views

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  get 'sign_up/index'
+  get 'login/index'
+  root 'login#index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  #
+end
