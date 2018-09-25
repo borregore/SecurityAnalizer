62780bbc73e29e8433c71e352b5d1e5bacadf461
gmotzespina <gmotzespina@icloud.com>
2018-09-24 22:46:49 -0500

    Add devise and postgres configuration

--- Gemfile
+++ Gemfile
@@ -34,6 +34,7 @@ gem 'jbuilder', '~> 2.5'
 # gem 'capistrano-rails', group: :development
 #
 gem 'devise'
+gem 'pg'
 
 
 gem 'materialize-sass', '~> 1.0.0'
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+
+  devise_for :users
   get 'sign_up/index'
   get 'login/index'
-  root 'login#index'
+  root to: 'login#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   #
